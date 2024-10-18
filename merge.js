894d5de5c6152d6584ba2f983e443b147229a327
Agnes <agnes.mccall5@gmail.com>
2024-10-17 21:53:11 -0400

    create merge fuction

--- merge.js
+++ merge.js
@@ -1,4 +1,31 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let result = [];
+
+    let i = 0, j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // Append remaining elements from arr1
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    // Append remaining elements from arr2
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
 
 function mergeSort() {}
 
